059ce71021c922a530c0516300234585736c232a
Hoàng Kim Phú <hoangkimphu84@gmail.com>
2023-08-28 19:21:42 +0700

    update api url

--- lib/core/initialize/core_url.dart
+++ lib/core/initialize/core_url.dart
@@ -1,9 +1,9 @@
 class CoreUrl {
-  static const baseURL = 'https://omsapi.hisoft.vn/api';
-  static const baseImageURL = 'https://omsapi.hisoft.vn/';
-  static const baseCateURL = 'https://omsapi.hisoft.vn/category/';
-  static const baseAvaURL = 'https://omsapi.hisoft.vn/user/';
+  static const baseURL = 'https://taskuatapi.hisoft.vn/api';
+  static const baseImageURL = 'https://taskuatapi.hisoft.vn/';
+  static const baseCateURL = 'https://taskuatapi.hisoft.vn/category/';
+  static const baseAvaURL = 'https://taskuatapi.hisoft.vn/user/';
 
-  static const baseApiUrl = 'https://omsapi.hisoft.vn/';
-  static const baseSignalrUrl = 'https://omsapi.hisoft.vn/notificationHub';
+  static const baseApiUrl = 'https://taskuatapi.hisoft.vn/';
+  static const baseSignalrUrl = 'https://taskuatapi.hisoft.vn/notificationHub';
 }
